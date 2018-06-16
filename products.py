83ed2418f51147ae52e9ac852fb24914e0567fc2
MatyHuang <greenstimorol@gmail.com>
2018-06-16 08:05:13 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,21 @@
-#讀取檔案
+import os
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+
+#檢察檔案是否存在
+if os.path.isfile('products.csv'):
+	print('yeah!找到檔案了！')
+	#讀取檔案
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)	
+else:
+	print('找不到檔案！')
+
+
 #使用者輸入資料
 while True:
 	name = input('請輸入商品名稱：')
@@ -15,9 +24,11 @@ while True:
 	price = input('請輸入商品價格：')
 	products.append([name, price])
 print(products)
+
 #印出購買資料
 for p in products:
 	print(p[0], '的價錢是', p[1], '元')
+
 #寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
